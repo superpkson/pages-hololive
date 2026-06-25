e9afd031ab3055fcd9828616613fc14c93ce3b9c
superpkson <superpkson@gmail.com>
2026-06-25 10:50:30 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+	let table = new DataTable('#orderedTable', {
+		ajax: {
+			url: 'data.json',
+			dataSrc: ''
+		},
+		columnDefs: [
+			{
+				render: (data, type, row) => `<a href="${row.img}" data-lightbox="group"><img src="${row.img}" class="img-fluid img-thumbnail" alt="${row.title}" style="min-width:70px; max-height:130px;"></a>`,
+				targets: 0,
+				orderable: false,
+			},
+			{
+				render: (data, type, row) => `<h6><a href="${row.href}">${row.title}</a></h6><p>${row.item}</p><small>#${row.order}</small>`,
+				targets: 1
+			},
+			{
+				render: (data, type, row) => `<p class="small">${row.unitPrice} *${row.amount}</p><p class="small">=${row.total}</p>`,
+				targets: 2,
+				orderable: false,
+			}
+		],
+		order: [[1, "asc"]]
+	});
+});
